f9efec9be71942a1db43383af015c7d572ab686d
phant80m <black.pandazayd@gmail.com>
2023-07-16 15:33:31 +1000

    push to crates.io

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use macro_colors::*;
 fn main() {
     println!("Hello, world!");
+    // Macro to print with green background color and bold style
     let x = 3;
     bold_green_println!("hello world {}", x);
 }
